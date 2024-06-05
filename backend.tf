7e2b326a9fe6ed27abcf5572ba3551526a5ee338
Prasanna Babu <prasannababuvijjana@gmail.com>
2024-06-05 13:43:17 +0530

    wefw

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "s3-revhire-backend-user-remote-new-auto"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "revhire-user-backend-dynamodb-new-auto"
+  }
+}
